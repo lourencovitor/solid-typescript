25264ac6f3989cd0e3a0fce5261346c53f624dc2
Vitor Lourenço <vitor.brother17@outlook.com.br>
2021-10-30 14:21:40 -0300

    feat: OCP open/close principle implemented

---
+++ src/ocp/classes/discount.ts
@@ -0,0 +1,27 @@
+// Strategy GoF
+
+export abstract class Discount {
+  abstract calculate(value: number): number;
+}
+
+export class FiftyPercentDiscount extends Discount {
+  private readonly discount: number = 0.5;
+
+  calculate(price: number): number {
+    return price - price * this.discount;
+  }
+}
+
+export class TenPercentDiscount extends Discount {
+  private readonly discount: number = 0.1;
+
+  calculate(price: number): number {
+    return price - price * this.discount;
+  }
+}
+
+export class NoDiscount extends Discount {
+  calculate(price: number): number {
+    return price;
+  }
+}
